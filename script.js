e9b96c7a3aae2929dde2946137afdcacb76a7afe
DjovannyP <pierredjovanny77@gmail.com>
2024-01-11 16:27:21 -0400

    Feat: refactor of the code
    the last feature was add but the program contain a problem to display
    the winner of the game. Score display before claiming who wins the game
    ( the computer or the player).

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 
+//computer choice
+const arrayChoice = ["rock", "paper", "scissors"];
 
-let arrayChoice = ["rock", "paper", "scissors"];
+//['Rock', 'Paper', 'Scissors]
+const buttons = document.querySelectorAll('button');
 
 function getComputerChoice(arr){
- let choice = Math.floor(Math.random() * arr.length); //random computer choice
+ let choice = Math.floor(Math.random() * arr.length); 
  return arrayChoice[choice];
 }
-
-const rockBtn = document.querySelector('#rock')
-const paperBtn = document.querySelector('#paper')
-const scissorsBtn = document.querySelector('#scissors')
+//display
 const result = document.querySelector('.result-screen h1');
 const playerScoreDisplay = document.querySelector('#player-score p');
-const computerScoreDisplay = document.querySelector('#computer-score p'); 
+const computerScoreDisplay = document.querySelector('#computer-score p');
+//player and computer selection
+let playerSelection;
+let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
 
@@ -20,7 +23,7 @@ let computerScore = 0;
 function playRound(playerSelection, computerSelection){ 
     if (playerSelection === "rock" && computerSelection === "paper") {
         computerScore ++;
-        return "You lose! Paper beats rock";
+        return "You lose! Paper beats rock ";
         
     } 
     else if (playerSelection === "paper" && computerSelection === "rock") {
@@ -43,33 +46,30 @@ function playRound(playerSelection, computerSelection){
         playerScore++;
         return "You win! Scissors beat Paper";
     } else {
-        return "TIES!";
+        return "It's a TIES!";
     }
 }
-//score display
 
-
-
-//player and computer selection
-let playerSelection;
-let computerSelection;
-
-
-const buttons = document.querySelectorAll('button');
-const buttonsArray = Array.from(buttons);
-
-for(let i = 0; i < buttonsArray.length; i++){
-    buttonsArray[i].addEventListener('click', (e) =>{
-        playerSelection = e.target.value;
-        computerSelection = getComputerChoice(arrayChoice);
-        result.textContent = playRound(playerSelection, computerSelection);
-        playerScoreDisplay.textContent = playerScore;
-        computerScoreDisplay.textContent = computerScore
-    
-    });
+function gameStop(){
+    if(playerScore > computerScore){
+        return "Congratulation! You win!";
+    }else{
+        return " You lose! Take your revenge!"
+    }
 }
 
-    //rock
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', (e) =>{
+        playerSelection = e.target.value;
+        computerSelection = getComputerChoice(arrayChoice);
+        result.textContent = playerScore == 5 || computerScore == 5 ? gameStop() : playRound(playerSelection, computerSelection);
+        playerScoreDisplay.textContent = playerScore;
+        computerScoreDisplay.textContent = computerScore;
+    });
+    
+}
+
+    
 
 
 
